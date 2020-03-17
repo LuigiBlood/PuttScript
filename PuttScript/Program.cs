12537f81170be7efcbb66bed4aca8aa672f33be1
LuigiBlood <luigiblood@gmail.com>
2020-03-17 02:11:16 +0100

    Add Encode functionality, optimize Decode a bit

--- PuttScript/Program.cs
+++ PuttScript/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -44,40 +45,77 @@ namespace PuttScript
             }
         }
 
+        static int GetDict(string tablefile, int order, out List<Tuple<string, string>> dict)
+        {
+            //Key = Hex, Value = UTF8
+
+            dict = new List<Tuple<string, string>>();
+            StreamReader table = File.OpenText(tablefile);
+            string test = "";
+            int line = 0;
+
+            while (!table.EndOfStream)
+            {
+                test = table.ReadLine();
+                if (!Regex.IsMatch(test, regexrule))
+                {
+                    Console.WriteLine("ERROR:\nCan't recognize at table line " + line + ": \"" + test + "\"");
+                    return 1;
+                }
+                //string[] value = Regex.Split(test, regexrule);
+                string[] value = test.Split('=', 2);
+                if ((value[0].Length & 1) == 1)
+                {
+                    Console.WriteLine("ERROR:\nTable line " + line + ": \"" + test + "\"");
+                    return 1;
+                }
+                string hex = value[0].ToUpperInvariant();
+                string text = value[1].Replace('\\', '\n');
+
+                if (order != 0)
+                    text = text.Trim('\n');
+
+                dict.Add(new Tuple<string, string>(hex, text));
+                line++;
+            }
+
+            table.Close();
+
+            dict.Sort(delegate(Tuple<string, string> x, Tuple<string, string> y)
+            {
+                string x_str = (order == 0) ? x.Item1 : x.Item2;
+                string y_str = (order == 0) ? y.Item1 : y.Item2;
+
+                if (x_str.Length != y_str.Length)
+                {
+                    return y_str.Length - x_str.Length;
+                }
+                else
+                {
+                    if (order != 0)
+                    {
+                        if (x_str.Contains("%"))
+                            return 1;
+                        else if (y_str.Contains("%"))
+                            return -1;
+                    }
+                    return -x_str.CompareTo(y_str);
+                }
+            });
+
+            foreach (Tuple<string, string> x in dict)
+                Console.WriteLine(x.Item1 + ":" + x.Item2);
+            return 0;
+        }
+
         static int Decode(string tablefile, string inputfile)
         {
             //Input File = Binary
 
-            List<Tuple<string, string>> dict = new List<Tuple<string, string>>();
-            //Key = Hex, Value = UTF8
-
-            //Set up Dictionary
-            {
-                StreamReader table = File.OpenText(tablefile);
-                string test = "";
-                int line = 0;
-
-                while (!table.EndOfStream)
-                {
-                    test = table.ReadLine();
-                    if (!Regex.IsMatch(test, regexrule))
-                    {
-                        Console.WriteLine("ERROR:\nCan't recognize at table line " + line + ": \"" + test + "\"");
-                        return 1;
-                    }
-                    string[] value = Regex.Split(test, regexrule);
-                    if ((value[0].Length & 1) == 1)
-                    {
-                        Console.WriteLine("ERROR:\nTable line " + line + ": \"" + test + "\"");
-                        return 1;
-                    }
-                    Console.WriteLine(value.Length + " : " + value[1].ToUpperInvariant() + " : " + value[2]);
-                    dict.Add(new Tuple<string, string>(value[1].ToUpperInvariant(), value[2].Replace('\\', '\n')));
-                    line++;
-                }
-
-                table.Close();
-            }
+            List<Tuple<string, string>> dict;
+            int DictError = GetDict(tablefile, 0, out dict);
+            if (DictError != 0)
+                return DictError;
 
             //Convert to Text
             FileStream input_f = File.OpenRead(inputfile);
@@ -89,75 +127,48 @@ namespace PuttScript
             input_f.Read(input, 0, length);
             input_f.Close();
 
+            Console.WriteLine("");
             for (int i = 0; i < input.Length;)
             {
-                List<int> indexes = new List<int>();
-                //Search all keys that starts with this byte
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.WriteLine("Progress: " + Math.Ceiling((double)i / input.Length * 100d) + "%  ");
+                int index_use = -1;
+
+                //Assume the list contains the longest first, therefore it should find the match without necessarily going through the entire list
                 for (int j = 0; j < dict.Count; j++)
                 {
-                    if (dict[j].Item1.StartsWith(input[i].ToString("X2")))
+                    int score = 0;
+                    for (int k = 0; k < (dict[j].Item1.Length / 2); k++)
                     {
-                        indexes.Add(j);
+                        string byte_tbl = dict[j].Item1.Substring(k * 2, 2);
+                        string byte_in = input[i + k].ToString("X2");
+
+                        if (byte_tbl == "%%")
+                        {
+                            score++;
+                        }
+                        else if (byte_tbl == byte_in)
+                        {
+                            score += 2;
+                        }
+                        else
+                        {
+                            score = 0;
+                            break;
+                        }
+                    }
+
+                    if (score != 0)
+                    {
+                        index_use = j;
+                        break;
                     }
                 }
 
-                //Console.WriteLine(input[i].ToString("X2") + ":" + indexes.Count);
-
-                if (indexes.Count == 0)
+                if (index_use == -1)
                 {
-                    Console.WriteLine("ERROR:\nInput file offset 0x" + i.ToString("X") + ":" + input[i].ToString("X2") + " not defined in table");
+                    Console.WriteLine("ERROR:\nInput file offset 0x" + i.ToString("X") + ":" + input[i].ToString("X2") + "  " + input[i + 1].ToString("X2") + " " + input[i + 2].ToString("X2") + " " + input[i + 3].ToString("X2") + "... not defined in table");
                     return 1;
-                }
-
-                //See which one to use if there's more than one
-                int index_use = indexes[0];
-                if (indexes.Count > 1)
-                {
-                    List<int> scores = new List<int>();
-                    for (int j = 0; j < indexes.Count; j++)
-                    {
-                        int score = 0;
-                        for (int k = 0; k < (dict[indexes[j]].Item1.Length / 2); k++)
-                        {
-                            string byte_tbl = dict[indexes[j]].Item1.Substring(k * 2, 2);
-                            string byte_in = input[i + k].ToString("X2");
-
-                            if (byte_tbl == "%%")
-                            {
-                                score++;
-                            }
-                            else if (byte_tbl == byte_in)
-                            {
-                                score += 2;
-                            }
-                            else
-                            {
-                                score = 0;
-                                break;
-                            }
-                        }
-                        scores.Add(score);
-                    }
-
-                    int total_score = 0;
-                    int total_index = -1;
-                    for (int j = 0; j < scores.Count; j++)
-                    {
-                        if (total_score < scores[j])
-                        {
-                            //Console.WriteLine(total_score + " < " +scores[j]);
-                            total_score = scores[j];
-                            total_index = j;
-                        }
-                    }
-
-                    if (total_index == -1)
-                    {
-                        Console.WriteLine("ERROR:\nInput file offset 0x" + i.ToString("X") + ":" + input[i].ToString("X2") + " " + input[i + 1].ToString("X2") + " " + input[i + 2].ToString("X2") + " " + input[i + 3].ToString("X2") + "... not defined in table");
-                        return 1;
-                    }
-
-                    index_use = indexes[total_index];
                 }
 
                 //Deal with %%
@@ -187,7 +198,7 @@ namespace PuttScript
             text_f.Write(text_out);
             text_f.Close();
 
-            Console.WriteLine("Written");
+            Console.WriteLine("Written as " + Path.GetFileNameWithoutExtension(inputfile) + "_out.txt");
 
             return 0;
         }
@@ -196,8 +207,109 @@ namespace PuttScript
         {
             //Input File = Text
 
-            Dictionary<string, string> dict = new Dictionary<string, string>();
+            List<Tuple<string, string>> dict;
+            int DictError = GetDict(tablefile, 1, out dict);
+            if (DictError != 0)
+                return DictError;
             //Key = UTF8, Value = Hex
+
+            //Convert to Bin
+            string[] text_in = File.ReadAllLines(inputfile);
+            List<byte> bin_out = new List<byte>();
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < text_in.Length; i++)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.WriteLine("Progress: " + Math.Ceiling((double)i / text_in.Length * 100d) + "%  ");
+                //text_in[i] = text_in[i].Trim('\n');
+                for (int c = 0; c < text_in[i].Length;)
+                {
+                    int index_use = -1;
+
+                    for (int j = 0; j < dict.Count; j++)
+                    {
+                        int score = 0;
+
+                        if ((c + dict[j].Item2.Length) > text_in[i].Length)
+                            continue;
+
+                        if (!dict[j].Item2.Contains("%%") && (dict[j].Item2.Equals(text_in[i].Substring(c, dict[j].Item2.Length))))
+                        {
+                            score = 1;
+                        }
+                        else
+                        {
+                            for (int k = 0; k < dict[j].Item2.Length; k++)
+                            {
+                                string char_tbl = dict[j].Item2.Substring(k, 1);
+                                string chat_in = text_in[i].Substring(c + k, 1);
+
+                                if (char_tbl == "%")
+                                {
+                                    score++;
+                                }
+                                else if (char_tbl == chat_in)
+                                {
+                                    score += 2;
+                                }
+                                else
+                                {
+                                    score = 0;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (score != 0)
+                        {
+                            index_use = j;
+                            break;
+                        }
+                    }
+
+                    if (index_use == -1)
+                    {
+                        Console.WriteLine("ERROR:\nInput file line " + (i + 1).ToString() + ":\n\"" + text_in[i] + "\" not defined in table");
+                        Console.WriteLine("^".PadLeft(c + 1));
+                        return 1;
+                    }
+
+                    //Deal with %%
+                    string table_in = dict[index_use].Item2;
+                    string table_out = dict[index_use].Item1;
+                    string text_input = text_in[i].Substring(c, table_in.Length);
+
+                    //Console.WriteLine(table_in.Length + " : " + table_in + " - " + text_input.Length + " : " + text_input);
+
+                    while (table_out.Contains("%%"))
+                    {
+                        int index_in = table_in.IndexOf("%%");
+                        int index_out = table_out.IndexOf("%%");
+                        table_in = table_in.Remove(index_in, 2).Insert(index_in, text_input.Substring(index_in, 2));
+                        table_out = table_out.Remove(index_out, 2).Insert(index_out, text_input.Substring(index_in, 2));
+                    }
+                    /*
+                    if (!table_in.Equals(text_input))
+                    {
+                        Console.WriteLine("ERROR:\nInput file line " + (i + 1).ToString() + ":\n\"" + text_in[i] + "\" problem");
+                        Console.WriteLine("^".PadLeft(c + 1));
+                        return 1;
+                    }*/
+
+                    for (int b = 0; b < table_out.Length / 2; b++)
+                        bin_out.Add(Byte.Parse(table_out.Substring(b * 2, 2), NumberStyles.HexNumber));
+                    c += text_input.Length;
+                }
+            }
+
+            FileStream bin_f = File.OpenWrite(Path.GetFileNameWithoutExtension(inputfile) + "_out.bin");
+            bin_f.Write(bin_out.ToArray(), 0, bin_out.Count);
+            bin_f.Close();
+
+            Console.WriteLine("Written as " + Path.GetFileNameWithoutExtension(inputfile) + "_out.bin");
+
             return 0;
         }
     }
